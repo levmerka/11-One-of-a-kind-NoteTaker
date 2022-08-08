c6dfa660757205140c07360cff403a972cc177ba
sl00p <lev@merka.us>
2022-08-08 15:52:04 -0700

    working on index.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const path = require('path');
 const api = require('./routes/index.js');
-const fs = require('fs');
-
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -23,10 +22,6 @@ app.get('/', (req, res) =>
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
 );
-
-
-
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
